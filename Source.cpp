7c4f9a8b0f0869e323c9cd45f9803a878fc10cd9
dungngo4520 <dungngo4520@gmail.com>
2022-05-05 17:35:57 +0700

    upload

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@
 
 int main() {
 	while (_getch() != 27) {
-		STARTUPINFO si;
+		STARTUPINFOA si;
 		PROCESS_INFORMATION pi;
 		ZeroMemory(&si, sizeof(si));
 		si.cb = sizeof(si);
@@ -19,23 +19,25 @@ int main() {
 			printf("FreeLibrary failed. Error: %d\n", GetLastError());
 		}
 
-		if (CreateProcessA(NULL,"C:\\Windows\\System32\\notepad.exe", NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi)) {
+		if (!CreateProcessA(NULL, "C:\\Windows\\System32\\notepad.exe", NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi)) {
 			printf("CreateProcessA failed. Error: %d\n", GetLastError());
 		}
-		if (TerminateProcess(pi.hProcess, 0)) {
+		if (!TerminateProcess(pi.hProcess, 0)) {
 			printf("TerminateProcess failed. Error: %d\n", GetLastError());
 		}
 		HANDLE hFile = CreateFileA("temp", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 		if (hFile == INVALID_HANDLE_VALUE) {
 			printf("CreateFileA failed. Error: %d\n", GetLastError());
 		}
-		if (WriteFile(hFile, "test", 4, NULL, NULL)) {
+		if (!WriteFile(hFile, "test", 4, NULL, NULL)) {
 			printf("WriteFile failed. Error: %d\n", GetLastError());
 		}
-		if (ReadFile(hFile, NULL, 4, NULL, NULL)) {
+		DWORD fileSize = GetFileSize(hFile, NULL), byteRead = 0;
+		char* buffer = (char*)malloc(fileSize);
+		if (!ReadFile(hFile, buffer, fileSize, &byteRead, NULL)) {
 			printf("ReadFile failed. Error: %d\n", GetLastError());
-					}
-		if (CloseHandle(hFile)) {
+		}
+		if (!CloseHandle(hFile)) {
 			printf("CloseHandle failed. Error: %d\n", GetLastError());
 		}
 		if (RegSetValueA(HKEY_CURRENT_USER, "test", REG_SZ, "test", 0) != ERROR_SUCCESS) {
